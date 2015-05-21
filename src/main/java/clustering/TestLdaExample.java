7d43e8d12747e4d0ae68c68a7bc92245ab691f36
Yufan Liu <liuyufansszx@hotmail.com>
2015-05-21 13:24:58 -0700

    refactor the name

--- src/main/java/clustering/TestLdaExample.java
+++ src/main/java/clustering/TestLdaExample.java
@@ -1,4 +1,4 @@
-package spark;
+package clustering;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/clustering/TestWordCount.java
+++ src/main/java/clustering/TestWordCount.java
@@ -1,4 +1,4 @@
-package spark;
+package clustering;
 
 import java.util.Arrays;
 import java.util.List;
