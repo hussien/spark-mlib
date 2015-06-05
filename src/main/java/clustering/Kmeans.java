d0fc25d068ae2f14fc4b0dec2c0e0efa68dbea14
Yufan Liu <liuyufansszx@hotmail.com>
2015-06-05 10:15:54 -0700

    demo for k-means clustering

---
+++ src/main/java/clustering/Kmeans.java
@@ -0,0 +1,113 @@
+package clustering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+
+
+
+public class Kmeans {
+
+
+	public static JavaRDD<Vector> loadData(JavaRDD<String> data){
+		
+		JavaRDD<Vector> parsedData = data.map(
+				new Function<String, Vector>() {
+					public Vector call(String s) {
+						String[] sarray = s.split(" ");
+						double[] values = new double[sarray.length];
+						for (int i = 0; i < sarray.length; i++)
+							values[i] = Double.parseDouble(sarray[i]);
+						return Vectors.dense(values);
+					}
+				}
+				);
+
+		return parsedData;
+
+	}
+
+	
+	public static JavaRDD<Vector> loadPoint(JavaSparkContext sc, List<Double[]> pointList ){
+		JavaRDD<Vector> parsedData = sc.parallelize(pointList).map(
+				new Function<Double[], Vector>() {
+					public Vector call(Double[] d) {
+						double[] values = new double[d.length];
+						for (int i = 0; i < d.length; i++)
+							values[i] = d[i];
+						return Vectors.dense(values);
+					}
+				}
+				);
+
+		return parsedData;
+
+	}
+	
+	
+	public static List<double[]> getCenters(JavaSparkContext sc, JavaRDD<Vector> parsedData, int numClusters, int numIterations){
+
+		KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
+		List<double[]> newList =new ArrayList<double[]>();
+		System.out.print("center(s): ");
+		for(Vector k: clusters.clusterCenters()){
+			newList.add(k.toArray());
+		}
+		return newList;
+		
+
+	}
+	
+	
+
+
+	public static void run(JavaSparkContext sc, JavaRDD<Vector> parsedData, int numClusters, int numIterations){
+
+		KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
+		
+		System.out.print("center(s): ");
+		for(Vector k: clusters.clusterCenters()){
+			System.out.print(k.toString()+" , ");
+		}
+		
+		System.out.println();
+		// Evaluate clustering by computing Within Set Sum of Squared Errors
+		double WSSSE = clusters.computeCost(parsedData.rdd());
+		System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("K-means Example").setMaster("local");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+        List <Double[]> points =new ArrayList<Double[]>(); 
+		// Load and parse data
+		String path = "data/kmeans_data.txt";
+		JavaRDD<String> data = sc.textFile(path);
+		JavaRDD<Vector> parsedData = loadData(data);
+		
+		parsedData.cache();
+		Double[] a = {1.0, 3.0};
+		Double[] b = {2.0, 4.0};
+		points.add(a);
+		points.add(b);
+		JavaRDD<Vector> pointData = loadPoint(sc, points);
+
+		// Cluster the data into two classes using KMeans
+		int numClusters = 2;
+		int numIterations = 20;
+		
+		run(sc, pointData, 2, 10);
+	
+	}
+
+}
---
+++ src/main/java/visualization/DrawingPoints.java
@@ -0,0 +1,189 @@
+package visualization;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Vector;
+
+import clustering.Kmeans;
+
+
+public class DrawingPoints
+{
+	public static void main(String[] args)
+	{
+
+
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		PointPanel myPanel =new PointPanel();
+
+		f.getContentPane().add(myPanel);
+		f.setSize(400,400);
+
+		f.setLocation(200,200);
+		f.setVisible(true);
+	}
+}
+
+class PointPanel extends JPanel
+{
+	List<Ellipse2D> pointList;
+	List<Ellipse2D> ovalList;
+	Color selectedColor;
+	Ellipse2D selectedPoint;
+
+
+
+	public PointPanel()
+	{
+
+		final SparkConf conf = new SparkConf().setAppName("K-means Example").setMaster("local");
+		final JavaSparkContext  sc = new JavaSparkContext(conf);
+
+
+		pointList = new ArrayList<Ellipse2D>();
+		ovalList = new ArrayList<Ellipse2D>(); 
+		selectedColor = Color.red;
+		addMouseListener(new PointLocater(this));
+		setBackground(Color.white);
+
+		JButton mybutton = new JButton("TEST");
+		JButton mybutton2 = new JButton("CLEAN");
+		mybutton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ovalList.clear();
+				List<Double[]>points =new ArrayList<Double[]>();
+				for(Ellipse2D p:pointList ){
+					Double [] a=new Double[2];
+					a[0] = p.getX();
+					a[1] = p.getY();
+					points.add(a);
+				}
+
+				JavaRDD<Vector> pointData = Kmeans.loadPoint(sc, points);
+
+				List<double[]> rs = Kmeans.getCenters(sc, pointData, 2, 10);
+
+				for(double[]point:rs){
+					//System.out.println(point[0]+" "+point[1]);
+					Ellipse2D e2 = new Ellipse2D.Double(point[0],point[1], 14, 14);
+					ovalList.add(e2);
+				}
+				repaint();
+			}          
+		});
+
+		mybutton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				pointList.clear();
+				ovalList.clear();
+
+				repaint();
+			}          
+		});
+
+		this.add(mybutton);
+		this.add(mybutton2);
+	}
+
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		Ellipse2D e;
+		Color color;
+		for(int j = 0; j < pointList.size(); j++)
+		{
+			e = (Ellipse2D)pointList.get(j);
+			if(e == selectedPoint)
+				color = selectedColor;
+			else
+				color = Color.blue;
+			g2.setPaint(color);
+			g2.fill(e);
+		}
+
+		for(int j = 0; j < ovalList.size(); j++)
+		{
+			e = (Ellipse2D)ovalList.get(j);
+			if(e == selectedPoint)
+				color = selectedColor;
+			else
+				color = Color.RED;
+			g2.setPaint(color);
+			g2.drawOval((int)e.getX(), (int)e.getY(), 32, 32);
+		}
+
+
+	}
+
+	public List getPointList()
+	{
+		return pointList;
+	}
+
+	public void setSelectedPoint(Ellipse2D e)
+	{
+		selectedPoint = e;
+		repaint();
+	}
+
+	public void addPoint(Point p)
+	{
+		Ellipse2D e = new Ellipse2D.Double(p.x - 3, p.y - 3, 6, 6);
+		pointList.add(e);
+		selectedPoint = null;
+		repaint();
+	}
+}
+
+class PointLocater extends MouseAdapter
+{
+	PointPanel pointPanel;
+
+	public PointLocater(PointPanel pp)
+	{
+		pointPanel = pp;
+	}
+
+	public void mousePressed(MouseEvent e)
+	{
+		Point p = e.getPoint();
+		boolean haveSelection = false;
+		List list = pointPanel.getPointList();
+		Ellipse2D ellipse;
+		for(int j = 0; j < list.size(); j++)
+		{
+			ellipse = (Ellipse2D)list.get(j);
+			if(ellipse.contains(p))
+			{
+				pointPanel.setSelectedPoint(ellipse);
+				haveSelection = true;
+				break;
+			}
+		}
+		if(!haveSelection)
+			pointPanel.addPoint(p);
+	}
+}
